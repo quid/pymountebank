b0b562bd0483cdecdfe86b945552d81ff34e4bea
Robert Roose <rroose@quid.com>
2017-03-21 10:54:21 -0700

    Bump version: 1.0.3 → 1.0.4

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("requirements.txt") as requirements_file:
 
 setup(
     name="pymountebank",
-    version="1.0.3",
+    version="1.0.4",
     author="Quid Inc",
     author_email="infrastructure@quid.com",
     packages=find_packages(exclude=["tests*"]),
