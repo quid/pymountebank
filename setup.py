6fc6492d55787115a4b826e662e93017d77c0ec1
Robert Roose <rroose@quid.com>
2017-03-24 16:00:26 -0700

    Added extra information to pip

--- setup.py
+++ setup.py
@@ -11,16 +11,28 @@ with open("requirements.txt") as requirements_file:
 
 setup(
     name="pymountebank",
+    description="Mountebank wrapper for mocking HTTP APIs",
     license="BSD 3-Clause License",
     version="1.0.4",
     author="Quid Inc",
     author_email="infrastructure@quid.com",
+    url="https://github.com/quid/pymountebank",
     packages=find_packages(exclude=["tests*"]),
     include_package_data=True,
-    description="Mountebank wrapper for mocking HTTP APIs",
     long_description=readme,
     install_requires=requirements,
     classifiers=[
+        "Intended Audience :: Developers",
         "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
     ]
 )
