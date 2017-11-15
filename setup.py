7529be136f85f22d78d56ba84ace2e53d6663dac
Jeffrey Starker <jstarker@quid.com>
2017-11-15 11:27:56 -0800

    Bump version: 1.2.0 → 1.3.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     name="pymountebank",
     description="Mountebank wrapper for mocking HTTP APIs",
     license="BSD 3-Clause License",
-    version="1.2.0",
+    version="1.3.0",
     author="Quid Inc",
     author_email="infrastructure@quid.com",
     url="https://github.com/quid/pymountebank",
